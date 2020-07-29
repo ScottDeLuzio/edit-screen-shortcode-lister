a404d03751f4bb944a321cc0e04e357c8873fd72
Ricardo Dominguez <ricardo@thoughtcatalog.com>
2020-07-29 15:51:39 -0400

    updating code as per phpcs

--- includes/get-shortcodes.php
+++ includes/get-shortcodes.php
@@ -1,9 +1,12 @@
 <?php
-if ( !defined( 'ABSPATH' ) ) {
-    die( "Sorry, you are not allowed to access this page directly." );
+/**
+ * Get Global Shortcodes
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Sorry, you are not allowed to access this page directly.' );
 }
 add_filter( 'media_buttons', 'shortcode_lister_menu', 15 );
-function shortcode_lister_menu( $location ){
+function shortcode_lister_menu( $location ) {
 	// retrieve registered shortcode tags
 	global $shortcode_tags;
 	
@@ -15,10 +18,15 @@ function shortcode_lister_menu( $location ){
 
 	switch ( $location ) {
 		case 'settings':
-			foreach ( $shortcode_tags as $code => $function ){ ?>
+			foreach ( $shortcode_tags as $code => $function ) { ?>
 				<tr valign="top">
 					<td>
-						<input id="shortcode_lister_settings[<?php echo $code; ?>]" name="shortcode_lister_settings[<?php echo $code; ?>]" type="checkbox" value="1" <?php if ( is_array($shortcode_lister_settings) && array_key_exists( $code, $shortcode_lister_settings ) ) { checked(1, $shortcode_lister_settings[ $code ]); } ?> />
+						<input id="shortcode_lister_settings[<?php echo $code; ?>]" name="shortcode_lister_settings[<?php echo $code; ?>]" type="checkbox" value="1" 
+							<?php 
+							if ( is_array( $shortcode_lister_settings ) && array_key_exists( $code, $shortcode_lister_settings ) ) {
+								checked( 1, $shortcode_lister_settings[ $code ] ); } 
+							?>
+						/>
 						<label class="description" for="shortcode_lister_settings[<?php echo $code; ?>]">[<?php echo $code; ?>]</label>
 					</td>
 				</tr>
@@ -28,7 +36,7 @@ function shortcode_lister_menu( $location ){
 		
 		default:
 			// loop through each shortcode tag to create array of tags
-			foreach( $shortcode_tags as $code => $function ){
+			foreach ( $shortcode_tags as $code => $function ) {
 				if ( is_array( $shortcode_lister_settings ) && array_key_exists( $code, $shortcode_lister_settings ) && 1 == $shortcode_lister_settings[ $code ] ) {
 					// do nothing
 				} else {
@@ -36,7 +44,7 @@ function shortcode_lister_menu( $location ){
 				}
 			}
 			echo '&nbsp;<select id="sl_select"><option class="noclick">Shortcodes</option>';
-			foreach ( $includes as $include ){
+			foreach ( $includes as $include ) {
 				$shortcodes_list .= '<option value="' . $include . '">' . $include . '</option>';
 			}
 			echo $shortcodes_list;
--- includes/scripts-styles.php
+++ includes/scripts-styles.php
@@ -1,8 +1,23 @@
 <?php
-if ( !defined( 'ABSPATH' ) ) {
-    die( "Sorry, you are not allowed to access this page directly." );
+/**
+ * Enqueue Style Scripts for Shortcode Lister
+ * 
+ * @wordpress-plugin
+ * @package Shortcode_Lister
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'Sorry, you are not allowed to access this page directly.' );
 }
-add_action( 'admin_enqueue_scripts', 'shortcode_lister_scripts');
-function shortcode_lister_scripts(){
-	wp_enqueue_script( 'shortcode-select', SHORTCODE_LISTER_PLUGIN_URL . '/includes/js/shortcode-lister.js', SHORTCODE_LISTER_VERSION, true );
+add_action( 'admin_enqueue_scripts', 'shortcode_lister_scripts' );
+
+/**
+ * Shortcode Lister Scripts
+ * 
+ * Enqueues Style Scripts
+ * 
+ * @return void
+ */
+function shortcode_lister_scripts() {
+	wp_enqueue_script( 'shortcode-select', SHORTCODE_LISTER_PLUGIN_URL . '/includes/js/shortcode-lister.js', array(), SHORTCODE_LISTER_VERSION, true );
 }
